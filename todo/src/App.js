64a5ac89ca28ec2f9386c98e5699e6d11d24ae57
Temp <temp@tmp.com>
2021-05-09 17:20:40 +0900

    Class component Based.

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,83 +1,234 @@
 import './App.css';
-import React, { Component,setState } from 'react'
+import React from 'react'
+import uuid from 'react-uuid';
 
 class TodoApp extends React.Component {
   constructor() {
     super()
     this.state = {
-      showTodos: true
+      displayType: "All", 
+      todoListElements: [
+        {
+          "id": uuid(), 
+          "name": "Learn React",
+          "complete": true 
+        },
+        {
+          "id": uuid(), 
+          "name": "Learn React2",
+          "complete": false 
+        },
+      ],
     }
     this.handleToggleTodoList = this.handleToggleTodoList.bind(this);
+    this.handleNewTodo = this.handleNewTodo.bind(this);
+    this.handleDone = this.handleDone.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleDisplayType = this.handleDisplayType.bind(this);
+    this.handleClearCompleted = this.handleClearCompleted.bind(this);
   }
-  handleToggleTodoList(e) {
-    e.preventDefault();
-    this.setState({ showTodos: !this.state.showTodos })
-    console.log(e);
+  //activeCount = this.state.todoListElements.filter((todo) => todo.complete===true).length;
+  handleToggleTodoList(all) {
+    console.log(all)
+    const   newTodos = this.state.todoListElements.map((todo) => {
+          return Object.assign({}, todo, {
+            complete: all,
+          });
+       
+      });
+   
+    this.setState({
+      todoListElements: newTodos,
+    });
+  }
+  handleNewTodo(todo) {
+    console.log("New Todo added: " + todo);
+    this.setState({ todoListElements: [...this.state.todoListElements, {"id": uuid(),"name": todo, "complete": false}]})
+  }
+
+  handleDone(id){
+    const newTodos = this.state.todoListElements.map((todo) => {
+      if (todo.id === id) {
+        return Object.assign({}, todo, {
+          complete: !todo.complete,
+        });
+      } else {
+        return todo;
+      }
+    });
+    this.setState({
+      todoListElements: newTodos,
+    });
+  }
+
+  handleDelete(id){
+    const newTodos = this.state.todoListElements.filter((todo) => todo.id!==id );
+    this.setState({
+      todoListElements: newTodos,
+    });
+  }
+
+  handleDisplayType(type){
+    this.setState({displayType: type});
+  }
+
+  handleClearCompleted(){
+    console.log("Clear Completed!!!")
+    const newTodos = this.state.todoListElements.filter((todo) => todo.complete===false );
+    this.setState({
+      todoListElements: newTodos,
+    });
   }
   render() {
-    this.todoListElement = [
-      { "name": "Learn React", "complete": true },
-      { "name": "Learn React2", "complete": false },
-      { "name": "Learn React3", "complete": true }
-    ]
+    const total = this.state.todoListElements.length;
+    const activeCount = this.state.todoListElements.filter((todo) => todo.complete===false).length;
 
     return (
       <section className="todoapp">
-        <InputArea />
-      
+        <InputArea 
+          newtodo={this.handleNewTodo}
+          activeCount={activeCount}
+          total={total}
+        />
+          {total &&
           <TodoList
-            todoListElement={this.todoListElement}
+            todoListElements={this.state.todoListElements}
             handleToggleTodoList={this.handleToggleTodoList}
-            toggleListState={this.state.showTodos}
-          />
-        <Footer />
+            displayType={this.state.displayType}
+            handleDone={this.handleDone}
+            handleDelete={this.handleDelete}
+            activeCount={activeCount}
+            total={total}
+          />}
+        {total &&
+
+        <Footer 
+          handleDisplayType={this.handleDisplayType}
+          handleClearCompleted={this.handleClearCompleted}
+          todoListElements={this.state.todoListElements}
+          displayType={this.state.displayType}
+          activeCount={activeCount}
+          total={total}
+        />
+          }
       </section>
     );
   }
 }
 
 class InputArea extends React.Component {
+
+  constructor() {
+    super();
+    this.handleEnter = this.handleEnter.bind(this);
+  }
+
+  handleEnter(e) {
+    if (e.charCode === 13) {
+      this.props.newtodo(e.target.value); //Prop drilling
+      e.target.value='';
+    }
+  }
+ 
   render() {
     return (
       <header className="header">
         <h1>todos</h1>
-        <input className="new-todo" placeholder="What needs to bo done ?"></input>
+        <input className="new-todo" onKeyPress={this.handleEnter} placeholder="What needs to bo done ?"></input>
       </header>
     );
   }
 }
 
 class TodoList extends React.Component {
+  constructor(){
+    super();
+    this.handleDone = this.handleDone.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleToggleTodoList = this.handleToggleTodoList.bind(this);
+
+  }
+
+  handleDone(id) {
+    console.log("TODO List -- >")
+    this.props.handleDone(id);
+  }
+  handleDelete(id) {
+    this.props.handleDelete(id);
+  }
+  handleToggleTodoList(){
+    console.log("TOggleaa!!!" + this.props.total +" "+ this.props.activeCount)
+    if (this.props.activeCount === 0 && this.props.total!==0){
+      this.props.handleToggleTodoList(false);
+    }
+    else{
+      this.props.handleToggleTodoList(true);
+
+    }
+
+  }
+
   render() {
-    const todos = this.props.todoListElement.map((todo) => (
-      <Todo
-        key={todo.name}
-        name={todo.name}
-        complete={todo.complete}
-      />
-    ));
+        const todos = this.props.todoListElements.map((todo) => {
+          let flag = true;
+
+          if (this.props.displayType!=="All"){
+              if (this.props.displayType==="Completed"){
+                flag = todo.complete? true: false
+              }
+              else{
+                flag = !todo.complete? true: false
+              }
+          }
+
+          if (flag){
+            return (
+              <Todo
+                key={todo.id}
+                id={todo.id}
+                name={todo.name}
+                complete={todo.complete}
+                handleDone={this.handleDone}
+                handleDelete={this.handleDelete}
+              />
+            );
+          }
+
+          console.log(todo.id + " "+todo.complete + " "+this.props.displayType)
+          
+        
+      });
 
     return (
       <section className="main">
-        <input id="toggle-all" className="toggle-all" type="checkbox" onClick={this.props.handleToggleTodoList}></input>
+        <input id="toggle-all" className="toggle-all" type="checkbox" onClick={this.handleToggleTodoList}></input>
         <label htmlFor="toggle-all">Mark as Complete!</label>
-        {this.props.toggleListState &&
         <ul className="todo-list">
           {todos}
         </ul>
-        }
+        
       </section>
     );
   }
 }
 
 class Todo extends React.Component {
-  handleDone() {
-    console.log("Crossed Out!");
+  constructor(){
+    super();
+
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleDone = this.handleDone.bind(this);
   }
-  handleDelete() {
-    console.log("Delete Clicked!");
+
+  handleDone(e) {
+    this.props.handleDone(this.props.id);
   }
+  
+  handleDelete(e) {
+    e.preventDefault();
+    this.props.handleDelete(this.props.id)
+  }
+
   render() {
     return (
       <li className={this.props.complete ? "completed" : ""}>
@@ -92,23 +243,39 @@ class Todo extends React.Component {
 }
 
 class Footer extends React.Component {
+  constructor(){
+    super();
+    this.handleDisplayType = this.handleDisplayType.bind(this);
+    this.handleClearCompleted = this.handleClearCompleted.bind(this);
+  }
+
+  handleDisplayType(e){
+    // console.log(e.target.outerText)
+    this.props.handleDisplayType(e.target.outerText);
+  }
+
+  handleClearCompleted(){
+    console.log("Clear Completed!!");
+    this.props.handleClearCompleted()
+  }
+
   render() {
     return (
       <footer className="footer">
-        <span className="todo-count"><strong>0</strong> item left</span>
+        <span className="todo-count"><strong>{this.props.activeCount}</strong> item left</span>
         <ul className="filters">
           <li>
-            <a className="selected" href="#/">All</a>
+            <a className={this.props.displayType==="All"?"selected":""} href="#/" onClick={this.handleDisplayType}>All</a>
           </li>
           <li>
-            <a href="#/active">Active</a>
+            <a className={this.props.displayType==="Active"?"selected":""} href="#/active" onClick={this.handleDisplayType}>Active</a>
           </li>
           <li>
-            <a href="#/completed">Completed</a>
+            <a className={this.props.displayType==="Completed"?"selected":""} href="#/completed" onClick={this.handleDisplayType}>Completed</a>
           </li>
         </ul>
-        <button className="clear-completed">Clear completed</button>
-      </footer>
+        {this.props.total!==this.props.activeCount?<button className="clear-completed" onClick={this.handleClearCompleted}>Clear completed</button>:""}
+        </footer>
     );
   }
 }
